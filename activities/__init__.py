6609f4b3ab989022153266fbef1b413850626070
mecsabb <sabbadinimitchell@gmail.com>
2025-07-13 10:17:21 -0400

    fix imports

---
+++ activities/__init__.py
@@ -0,0 +1,19 @@
+from .run_scrape import run_scrape
+from .run_dspy import run_dspy  
+from .run_vdb import run_vdb
+from .run_scoop_preprocess import run_scoop_preprocess
+from .run_scoop_clustering import run_scoop_clustering
+from .run_cluster import run_cluster
+from .run_faq_batch import run_faq_batch
+from .run_enhanced_articles import run_enhanced_articles
+
+__all__ = [
+    'run_scrape',
+    'run_dspy', 
+    'run_vdb',
+    'run_scoop_preprocess',
+    'run_scoop_clustering',
+    'run_cluster',
+    'run_faq_batch',
+    'run_enhanced_articles',
+]
--- workflows/article_generation.py
+++ workflows/article_generation.py
@@ -1,12 +1,5 @@
+from datetime import timedelta
 from temporalio import workflow
-from activities.run_scrape import run_scrape
-from activities.run_dspy import run_dspy
-from activities.run_vdb import run_vdb
-from activities.run_scoop_preprocess import run_scoop_preprocess
-from activities.run_scoop_clustering import run_scoop_clustering
-from activities.run_cluster import run_cluster
-from activities.run_faq_batch import run_faq_batch
-from activities.run_enhanced_articles import run_enhanced_articles
 from activities import (
     run_scrape, run_dspy, run_vdb,
     run_scoop_preprocess, run_scoop_clustering,
@@ -17,11 +10,35 @@ from activities import (
 class ArticleGenWorkflow:
     @workflow.run
     async def run(self) -> None:
-        await workflow.execute_activity(run_scrape)
-        await workflow.execute_activity(run_dspy)
-        await workflow.execute_activity(run_vdb)
-        await workflow.execute_activity(run_scoop_preprocess)
-        await workflow.execute_activity(run_scoop_clustering)
-        await workflow.execute_activity(run_cluster)
-        await workflow.execute_activity(run_faq_batch)
-        await workflow.execute_activity(run_enhanced_articles)
+        await workflow.execute_activity(
+            run_scrape,
+            start_to_close_timeout=timedelta(hours=2)
+        )
+        await workflow.execute_activity(
+            run_dspy,
+            start_to_close_timeout=timedelta(hours=2)
+        )
+        await workflow.execute_activity(
+            run_vdb,
+            start_to_close_timeout=timedelta(hours=2)
+        )
+        await workflow.execute_activity(
+            run_scoop_preprocess,
+            start_to_close_timeout=timedelta(hours=2)
+        )
+        await workflow.execute_activity(
+            run_scoop_clustering,
+            start_to_close_timeout=timedelta(hours=2)
+        )
+        await workflow.execute_activity(
+            run_cluster,
+            start_to_close_timeout=timedelta(hours=2)
+        )
+        await workflow.execute_activity(
+            run_faq_batch,
+            start_to_close_timeout=timedelta(hours=2)
+        )
+        await workflow.execute_activity(
+            run_enhanced_articles,
+            start_to_close_timeout=timedelta(hours=2)
+        )
