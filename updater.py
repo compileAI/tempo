b2bb203956ba30194e6eee459f9bf4bfd6520d63
Mitchell <sabbadinimitchell@gmail.com>
2025-09-26 16:46:43 -0400

    change lambda to function

--- updater.py
+++ updater.py
@@ -3,15 +3,18 @@ from temporalio.client import Client, ScheduleSpec
 
 async def main():
     client = await Client.connect("localhost:7233")
+
+    def update_timezone(input):
+        input.description.schedule.spec.time_zone_name = "America/Toronto"
+        return input.description
     
     # update daily article gen workflow
     handle = client.get_schedule_handle("daily-article-gen-workflow")
-    # lambda is a hack to get the await
-    await handle.update(lambda _: handle.spec.time_zone_name == "America/Toronto")
+    await handle.update(update_timezone)
 
     # update daily stagehand workflow
     handle = client.get_schedule_handle("daily-stagehand-workflow")
-    await handle.update(lambda _: handle.spec.time_zone_name == "America/Toronto")
+    await handle.update(update_timezone)
 
 if __name__ == "__main__":
     asyncio.run(main())
