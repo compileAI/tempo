c2fd493225b7f77e2b81430e48f773d543ba3b73
Mitchell <sabbadinimitchell@gmail.com>
2025-09-26 16:44:43 -0400

    patch for async

--- updater.py
+++ updater.py
@@ -1,15 +1,17 @@
+import asyncio
 from temporalio.client import Client, ScheduleSpec
 
-def main():
-    client = Client.connect("localhost:7233")
+async def main():
+    client = await Client.connect("localhost:7233")
     
     # update daily article gen workflow
     handle = client.get_schedule_handle("daily-article-gen-workflow")
-    handle.spec.time_zone_name = "America/Toronto"
+    # lambda is a hack to get the await
+    await handle.update(lambda _: handle.spec.time_zone_name == "America/Toronto")
 
     # update daily stagehand workflow
     handle = client.get_schedule_handle("daily-stagehand-workflow")
-    handle.spec.time_zone_name = "America/Toronto"
+    await handle.update(lambda _: handle.spec.time_zone_name == "America/Toronto")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
